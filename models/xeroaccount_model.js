f617ab5bcdd023c7f74cecd7394bb89aa6dbb596
Jason Norwood-Young <jason@10layer.com>
2020-01-09 20:52:57 +0000

    Fix permissions for xeroaccounts

--- models/xeroaccount_model.js
+++ models/xeroaccount_model.js
@@ -7,15 +7,21 @@ var Location = require('./location_model');
 var XeroAccountSchema   = new Schema({
 	location_id: { type: ObjectId, ref: 'Location' },
 	consumer_key: String,
-	consumer_secret: String
+	consumer_secret: String,
+	client_id: String,
+	client_secret: String,
+	access_token: String,
+	tenant_id: String,
+	refresh_token: String,
+	token_expires: Date,
 }, {
 	timestamps: true
 });
 
 XeroAccountSchema.set("_perms", {
-	super_user: "crud",
-	admin: "r",
-	user: "r",
+	setup: "crud",
+	api: "r",
+	admin: "-"
 });
 
 module.exports = mongoose.model('XeroAccount', XeroAccountSchema);
