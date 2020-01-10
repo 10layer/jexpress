d14d6add8bfc0815cd6bab3d1beca929fe5a5297
Jason Norwood-Young <jason@10layer.com>
2020-01-10 00:59:28 +0000

    Make location_id unique

--- models/xeroaccount_model.js
+++ models/xeroaccount_model.js
@@ -5,15 +5,15 @@ var ObjectId = mongoose.Schema.Types.ObjectId;
 var Location = require('./location_model');
 
 var XeroAccountSchema   = new Schema({
-	location_id: { type: ObjectId, ref: 'Location' },
-	consumer_key: String,
-	consumer_secret: String,
-	client_id: String,
-	client_secret: String,
-	access_token: String,
-	tenant_id: String,
-	refresh_token: String,
-	token_expires: Date,
+	location_id: { type: ObjectId, ref: 'Location', unique: true },
+	consumer_key: String, // oAuth1
+	consumer_secret: String, // oAuth1
+	client_id: String, // oAuth2
+	client_secret: String, // oAuth2
+	access_token: String, // oAuth2
+	tenant_id: String, // oAuth2
+	refresh_token: String, // oAuth2
+	token_expires: Date, // oAuth2
 }, {
 	timestamps: true
 });
