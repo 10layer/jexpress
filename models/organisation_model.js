47cee17a7388db13f95e594d408ab3c36882d27b
Jason Norwood-Young <jason@10layer.com>
2020-03-11 17:02:15 +0000

    Do some clever shit right here - move the org model from front to back end

--- models/organisation_model.js
+++ models/organisation_model.js
@@ -1,22 +1,23 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+const mongoose     = require('mongoose');
+const Schema       = mongoose.Schema;
 
-// var monguurl = require("monguurl");
-var friendly = require("mongoose-friendly");
+const friendly = require("mongoose-friendly");
 
-var ObjectId = mongoose.Schema.Types.ObjectId;
-var Mixed = mongoose.Schema.Types.Mixed;
-var Membership = require('./membership_model');
-var XeroOrgAccount = require('./xeroorgaccount_model');
-var Location = require('./location_model');
-var User = require('./user_model');
-var diff = require('deep-diff').diff;
-var Log = require("./log_model");
-var messagequeue = require("../libs/messagequeue");
-var Organisation = require('./organisation_model');
-var IndustrySector = require("./industrysector_model");
+const ObjectId = mongoose.Schema.Types.ObjectId;
+const Mixed = mongoose.Schema.Types.Mixed;
+const Membership = require('./membership_model');
+const XeroOrgAccount = require('./xeroorgaccount_model');
+const Location = require('./location_model');
+const User = require('./user_model');
+const diff = require('deep-diff').diff;
+const Log = require("./log_model");
+const messagequeue = require("../libs/messagequeue");
+const Organisation = require('./organisation_model');
+const IndustrySector = require("./industrysector_model");
+const Lineitem = require("./lineitem_model");
+const Adhoc = require("./adhoc_model");
 
-var OrganisationSchema   = new Schema({
+const OrganisationSchema   = new Schema({
 	name: { type: String, unique: true, index: true },
 	legal_name: String,
 	urlid: { type: String, unique: true, index: true },
@@ -217,5 +218,83 @@ OrganisationSchema.post('save', function(doc) {
 OrganisationSchema.virtual("__user").set(function (user) {
 	this.sender = user;
 });
+
+function sortLineitems(lineitems) {
+	let is_sorted = false;
+	for (let lineitem of lineitems) {
+		if (lineitem.order) is_sorted = true;
+	}
+	if (is_sorted) {
+		lineitems.sort((a, b) => a.order - b.order);
+		return lineitems; // This invoice has already been ordered, bail
+	}
+	var products = [];
+	var licenses = [];
+	var others = [];
+	var primary = [];
+	while (lineitems.length) {
+		var lineitem = lineitems.pop();
+		if (lineitem.type === "product") {
+			products.push(lineitem);
+		} else if (lineitem.description && lineitem.description.indexOf("(Primary Member)") !== -1) {
+			primary.push(lineitem);
+		} else if (lineitem.type == "license") {
+			licenses.push(lineitem);
+		} else {
+			others.push(lineitem);
+		}
+	}
+	products.sort((a, b) => (b.price < a.price) ? -1 : 1);
+	licenses.sort((a, b) => (b.description > a.description) ? -1 : 1);
+	others.sort((a, b) => (b.description > a.description) ? -1 : 1);
+	return [].concat(products, others, primary, licenses);
+}
+
+const process_lineitem = lineitem => {
+	if (lineitem.product_id) {
+		lineitem.description = `${lineitem.product_id.name}${(lineitem.comment) ? `\n${lineitem.comment}` : ``}`;
+	}
+	lineitem.account_code = lineitem.account_code || "2000";
+	if (lineitem.product_id && lineitem.product_id.xero_account) lineitem.account_code = lineitem.product_id.xero_account;
+	if (lineitem.license_id && lineitem.license_id.membership_id && lineitem.license_id.membership_id.xero_account) lineitem.account_code = lineitem.license_id.membership_id.xero_account;
+	lineitem.type = "other";
+	lineitem.pro_rata = false;
+	if (lineitem.license_id) {
+		lineitem.type = "license";
+		lineitem.pro_rata = true;
+	} else if (lineitem.product_id) {
+		lineitem.type = "product";
+		lineitem.pro_rata = lineitem.product_id.pro_rata;
+	}
+	lineitem.xero_tax_type = (lineitem.product_id) ? lineitem.product_id.xero_tax_type : null;
+	lineitem.item_code = (lineitem.product_id) ? lineitem.product_id.xero_code : lineitem.xero_id;
+	return lineitem;
+}
+
+OrganisationSchema.statics.getFull = async function(opts) {
+	if (!opts.__user) {
+		return Promise.reject("User required");
+	}
+	const _id = opts.organisation_id;
+	// console.log(opts);
+	const Organisation = mongoose.model('Organisation', OrganisationSchema);
+	const organisation = await Organisation.findById(_id).populate("user_id").populate("location_id");
+	if (!opts.__user || !opts.__user.admin) {
+		if (organisation.user_id._id !== opts.__user._id) {
+			return Promise.reject("Admins and owners only");
+		}
+	}
+	let membership = null;
+	if (organisation.user_id && organisation.user_id.membership_id) {
+		membership = await Membership.findById(organisation.user_id.membership_id);
+	}
+	// console.log({ organisation_id: _id, _deleted: false });
+	const lineitems = sortLineitems((await Lineitem.find({ organisation_id: _id, _deleted: false })
+		.populate({ path: "license_id", populate: { path: "membership_id" } }).populate("product_id")).map(process_lineitem));
+	const adhoc_lineitems = sortLineitems((await Adhoc.find({ organisation_id: _id, _deleted: false })
+		.populate({ path: "license_id", populate: { path: "membership_id" } }).populate("product_id")).map(process_lineitem));
+	console.log("Lineitem Count:", lineitems.length);
+	return { ...organisation._doc, membership, lineitems, adhoc_lineitems };
+}
 
 module.exports = mongoose.model('Organisation', OrganisationSchema);
