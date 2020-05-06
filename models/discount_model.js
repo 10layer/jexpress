e6928bab0f52b280038117cf365b69795a610a8a
Jason Norwood-Young <jason@10layer.com>
2020-05-06 14:01:50 +0000

    Fix issue

--- models/discount_model.js
+++ models/discount_model.js
@@ -10,6 +10,7 @@ const diff         = require('deep-diff').diff;
 const Log          = require("./log_model");
 
 const oneYearFromNow = new Date();
+const twoYearsFromNow = new Date();
 oneYearFromNow.setFullYear(oneYearFromNow.getFullYear() + 1);
 twoYearsFromNow.setFullYear(oneYearFromNow.getFullYear() + 2);
 const oneYearAgo = new Date();
@@ -25,7 +26,7 @@ const DiscountSchema   = new Schema({
     lineitem_id: { type: ObjectId, ref: "LineItem" }, // If left blank, apply to entire organisation
     license_id: { type: ObjectId, ref: "License" },
     date_start: { type: Date, default: Date.now, max: '9999-12-31', min: '1800-01-01', required: true, index: true },
-    date_end: { type: Date, max: twoYearsFromNow, min: '1800-01-01', index: true },
+    date_end: { type: Date, max: twoYearsFromNow, min: '1800-01-01', required: true, index: true },
     apply_to: [ { type: String, validate: /product|license|booking|all/ } ], // For organisation-wide discounts
 	_owner_id: { type: ObjectId, ref: "User" },
 	_version: { type: Number, default: 0 },
