4617f793de02afc28a87109c2dcdca48353abe0b
Jason Norwood-Young <jason@10layer.com>
2020-05-18 07:53:57 +0000

    Bookable product types

--- models/producttype_model.js
+++ models/producttype_model.js
@@ -7,6 +7,9 @@ var ProductTypeSchema   = new Schema({
 	name: { type: String, required: true, index: true },
 	fire_action: String,
 	price_recommendation_formula: String,
+	bookable: { type: Boolean, default: false },
+	bookable_noun: String,
+	bookable_time_units: Number,
 	_owner_id: Objectid,
 	_deleted: { type: Boolean, default: false, index: true },
 }, {
