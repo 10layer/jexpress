1fdcb28a71e383e42d64abac2ffb508fe3bd97c7
Jason Norwood-Young <jason@10layer.com>
2020-06-03 06:06:59 +0000

    Enhance product type for bookable items

--- models/producttype_model.js
+++ models/producttype_model.js
@@ -1,19 +1,26 @@
-var mongoose     = require('mongoose');
-var Schema       = mongoose.Schema;
+const mongoose     = require('mongoose');
+const Schema       = mongoose.Schema;
+const Objectid = mongoose.Schema.Types.ObjectId;
+const friendly = require("mongoose-friendly");
 
-var Objectid = mongoose.Schema.Types.ObjectId;
-
-var ProductTypeSchema   = new Schema({
+const ProductTypeSchema   = new Schema({
 	name: { type: String, required: true, index: true },
+	urlid: { type: String, unique: true, index: true },
 	fire_action: String,
 	price_recommendation_formula: String,
 	bookable: { type: Boolean, default: false },
 	bookable_noun: String,
-	bookable_time_units: Number,
+	bookable_time_units: String,
+	img: String,
 	_owner_id: Objectid,
 	_deleted: { type: Boolean, default: false, index: true },
 }, {
 	timestamps: true
+});
+
+ProductTypeSchema.plugin(friendly, {
+	source: 'name',
+	friendly: 'urlid'
 });
 
 ProductTypeSchema.set("_perms", {
